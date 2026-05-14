c4fdbb9fd1ebb9f09fdcf1a9064eabf4b72f8b14
spagnuolocarmine <spagnuolocarmine@users.noreply.github.com>
2026-05-14 09:21:27 +0000

    deploy: b44a5f3557eb89a85e6ca30ee50ba8ec766d6977

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b3f10eb2.js',
+                path_to_root + 'searchindex-73ffc100.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
